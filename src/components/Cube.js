5f42319e71dc4fb847bee43b042fccb8b4adeecd
Andrew Liebchen <andrewliebchen@gmail.com>
2016-06-25 22:05:43 -0700

    Cube tweaks

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,17 +1,26 @@
 import React, { PropTypes } from 'react';
 import styles from '../styles/Cube.scss';
 
-const Primative = (props) =>
-  <div
-    className={props.anaglyph ? styles.anaglyphCube : styles.cube}
-    style={{transform: `translate3d(-50%, -50%, 0) rotateX(-${props.orbitX}deg) rotateY(${props.zAngle}deg) rotateZ(${props.orbitY}deg)`}}>
-    <div className={styles.frontFace}/>
-    <div className={styles.backFace}/>
-    <div className={styles.leftFace}/>
-    <div className={styles.rightFace}/>
-    <div className={styles.topFace}/>
-    <div className={styles.bottomFace}/>
-  </div>
+const Primative = (props) => {
+  const className = props.anaglyph ? styles.anaglyphCube : styles.cube;
+  const style = {
+    transform: `translate3d(-50%, -50%, 0) rotateX(-${props.orbitX}deg) rotateY(${props.zAngle}deg) rotateZ(${props.orbitY}deg)`
+  };
+  
+  return (
+    <div
+      className={className}
+      style={style}>
+      <div className={styles.frontFace}/>
+      <div className={styles.backFace}/>
+      <div className={styles.leftFace}/>
+      <div className={styles.rightFace}/>
+      <div className={styles.topFace}/>
+      <div className={styles.bottomFace}/>
+    </div>
+  );
+}
+
 
 const Cube = (props) =>
   <div className={styles.cubes}>
